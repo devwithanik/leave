4da8db6835f6ea4b261c3d7927fe15fb6229d460
devwithanik <136363350+devwithanik@users.noreply.github.com>
2026-02-10 00:49:45 +0600

    Remove saveBioId and requireLogin functions
    Removed saveBioId and requireLogin functions from app.js.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,6 @@
 console.log("app.js loaded");
 
-// Store Bio ID on login form submit
 function saveBioId() {
   const bioId = document.getElementById("bioId").value;
   localStorage.setItem("bioId", bioId);
-}
-
-// Redirect protection (optional)
-function requireLogin() {
-  if (!localStorage.getItem("bioId")) {
-    window.location.href = "index.html";
-  }
 }
