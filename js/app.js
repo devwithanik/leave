34a784c1223e7b46659de7302f638ca5556e0a15
devwithanik <136363350+devwithanik@users.noreply.github.com>
2026-02-10 03:34:33 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ function saveBioId() {
   const bioId = document.getElementById("bioId").value;
   localStorage.setItem("bioId", bioId);
 }
+
+function requireLogin() {
+  const bioId = localStorage.getItem("bioId");
+  if (!bioId) {
+    window.location.href = "index.html";
+  } else {
+    const input = document.getElementById("bioId");
+    if (input) input.value = bioId;
+  }
+}
